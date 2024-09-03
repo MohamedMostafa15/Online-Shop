05292ba753f7284258b56ca68096a27a3f624515
MohamedMostafa15 <m.hamada86850@gmail.com>
2024-09-03 13:22:40 +0300

    Initial Upload

---
+++ src/components/Notefound/Notefound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import style from "./Notefound.module.css"
+export default function Notefound() {
+  return <>
+  <h2 className='ml-96 mt-28 font-bold'> Page is Not Found</h2>
+  
+  </>
+}
